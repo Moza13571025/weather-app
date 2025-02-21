24a53acd7ef549f9b08ea97a8aa61b0fd1d1f2ab
Moza <moza13571025@gmail.com>
2025-02-22 01:47:01 +0800

    透過axios取得api資料

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      useEffect(()=> {
+      axios
+        .get(
+          "https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0005-001?Authorization=CWA-6D885349-B9CF-4B71-AAEA-5209ACCA0BEE"
+        )
+        .then((response)=> console.log("API回應資料", response.data))
+        .catch((error)=> console.error("API錯誤", error));
+    },[]);
+  return <h1>API 測試中，請開啟 Console 查看</h1>;
 }
 
 export default App;
